d9554972e61daa15203e8ddf4c4d1020b5c65f6f
David Vinazza <david.vinazza@gmail.com>
2017-08-27 12:16:45 -0300

    Mas prolijo

--- cupsAccounting/mailer/__init__.py
+++ cupsAccounting/mailer/__init__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
-from cupsAccounting.utils import objetoBase, enviarCorreo
+from cupsAccounting.utils import objetoBase
 from cupsAccounting.logger import Logger
 
 # from threading import Thread
 # from multiprocessing import Queue
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+from os.path import basename
 
 
 class Mailer(objetoBase, Logger):
@@ -13,43 +19,70 @@ class Mailer(objetoBase, Logger):
         self.logger.debug('Iniciando Mailer con configuración: %s' % config)
         self.config = config
 
+    def _enviarCorreo(self, correo):
+        self.logger.debug('Enviando Correo: %s' % correo['subject'])
+
+        msg = MIMEMultipart()
+        msg['From'] = correo['usuario']
+        msg['To'] = correo['to']
+        msg['Subject'] = correo['subject']
+        msg.attach(MIMEText(correo['contenido'], 'html'))
+
+        if "attachment" in correo:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(correo['attachment'], 'rb').read())
+            encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename="%s"' %
+                            basename(correo['attachment']))
+            msg.attach(part)
+
+        mailServer = SMTP(self.config['smtp_serv'], self.config['smtp_port'])
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(self.config['smtp_user'], self.config['smtp_pass'])
+        mailServer.sendmail(
+            correo['usuario'],
+            "%s@%s" % (correo['usuario'], correo['dominio']),
+            msg.as_string())
+        mailServer.close()
+
     def notificar(self, j, tipo):
 
         # algunos usuarios no reciben notificaciones porque no tienen mail
         if j.usuario in self.config['excluidos']:
             return
 
-        mail = {}  # Voy a armar un dict para pasarselo al worker
-        mail['config'] = self.config['smtp_config']
+        correo = {}  # Voy a armar un dict para pasarselo al worker
 
         if j.usuario in self.config['aliases']:
-            mail['to'] = self.config['aliases'][j.usuario]
+            correo['to'] = self.config['aliases'][j.usuario]
         elif "@" in j.usuario:
-            mail['to'] = j.usuario.split('@')  # Aca deberia ir el nombre
-            mail['usuario'], mail['dominio'] = j.usuario.split('@')
+            correo['to'] = j.usuario.split('@')  # Aca deberia ir el nombre
+            correo['usuario'], correo['dominio'] = j.usuario.split('@')
         else:
-            mail['usuario'] = j.usuario
-            mail['dominio'] = self.config['dominio']
-            mail['to'] = "%s@%s" % (j.usuario, self.config['dominio'])
+            correo['usuario'] = j.usuario
+            correo['dominio'] = self.config['dominio']
+            correo['to'] = "%s@%s" % (j.usuario, self.config['dominio'])
 
         if tipo == "received":  # Determino el subject
-            mail['subject'] = "Impresion Recibida: %s" % j.nombre
+            correo['subject'] = "Impresion Recibida: %s" % j.nombre
         elif tipo == "cancelled":
-            mail['subject'] = "Impresion Cancelada: %s" % j.nombre
+            correo['subject'] = "Impresion Cancelada: %s" % j.nombre
         elif tipo == "started":
-            mail['subject'] = "Impresion Iniciando: %s" % j.nombre
+            correo['subject'] = "Impresion Iniciando: %s" % j.nombre
         elif tipo == "ended":
-            mail['subject'] = "Impresion Finalizada: %s" % j.nombre
+            correo['subject'] = "Impresion Finalizada: %s" % j.nombre
 
         # if j.paginas != -1:
-        #    mail['contenido'] = j.paginas
+        #    correo['contenido'] = j.paginas
         # else:
-        #mail['contenido'] = j.mail_repr()
-        mail['contenido'] = mail['subject']
+        # correo['contenido'] = j.mail_repr()
+        # correo['contenido'] = mail['subject']
 
         try:
-            self.logger.debug('Enviando Correo: %s' % mail['subject'])
-            enviarCorreo(mail)
+            self._enviarCorreo(correo)
         except:
             self.logger.error('No pude enviar un correo')
         finally:
--- cupsAccounting/utils.py
+++ cupsAccounting/utils.py
@@ -2,12 +2,6 @@
 
 from pwd import getpwnam
 
-from smtplib import SMTP
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.encoders import encode_base64
-from os.path import basename
 
 u_black = ['Administrador', 'administrador', 'root', 'admin']
 u_white = ['marie']
@@ -33,32 +27,4 @@ def validarUsuario(user):
     return -1
 
 
-def enviarCorreo(correo):
 
-    msg = MIMEMultipart()
-    msg['From'] = correo['usuario']
-    msg['To'] = correo['to']
-    msg['Subject'] = correo['subject']
-    msg.attach(MIMEText(correo['contenido'], 'html'))
-
-    if "attachment" in correo:
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(open(correo['attachment'], 'rb').read())
-        encode_base64(part)
-        part.add_header('Content-Disposition',
-                        'attachment; filename="%s"' %
-                        basename(correo['attachment']))
-        msg.attach(part)
-
-    mailServer = SMTP(correo['config']['smtp_serv'],
-                      correo['config']['smtp_port'])
-    mailServer.ehlo()
-    mailServer.starttls()
-    mailServer.ehlo()
-    mailServer.login(correo['config']['smtp_user'],
-                     correo['config']['smtp_pass'])
-    mailServer.sendmail(
-        correo['usuario'],
-        "%s@%s" % (correo['usuario'], correo['dominio']),
-        msg.as_string())
-    mailServer.close()
